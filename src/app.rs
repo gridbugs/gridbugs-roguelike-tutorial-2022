ee8a52bab2814987302cc961ebec85b8c5e590fa
Stephen Sherratt <stephen@sherra.tt>
2022-09-09 20:47:36 +0200

    1.3: Player can move

--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,62 @@
 use gridbugs::{
     chargrid::{control_flow::*, prelude::*},
     coord_2d::{Coord, Size},
+    direction::CardinalDirection,
 };
+
+// An update to the game state
+enum GameAction {
+    Move(CardinalDirection),
+}
+
+// Associate game actions with input events
+fn game_action_from_input(input: Input) -> Option<GameAction> {
+    match input {
+        Input::Keyboard(keyboard_input) => {
+            use CardinalDirection::*;
+            use GameAction::*;
+            match keyboard_input {
+                KeyboardInput::Left => Some(Move(West)),
+                KeyboardInput::Right => Some(Move(East)),
+                KeyboardInput::Up => Some(Move(North)),
+                KeyboardInput::Down => Some(Move(South)),
+                _ => None,
+            }
+        }
+        _ => None,
+    }
+}
 
 // The state of the game
 struct GameData {
     player_coord: Coord,
+    screen_size: Size,
 }
 
 impl GameData {
     fn new(screen_size: Size) -> Self {
         // The player starts in the centre of the screen
         let player_coord = screen_size.to_coord().unwrap() / 2;
-        Self { player_coord }
+        Self {
+            player_coord,
+            screen_size,
+        }
+    }
+
+    // Move the player character one cell in the given direction
+    fn move_player(&mut self, direction: CardinalDirection) {
+        let new_player_coord = self.player_coord + direction.coord();
+        // Don't let the player walk off the screen
+        if new_player_coord.is_valid(self.screen_size) {
+            self.player_coord = new_player_coord;
+        }
+    }
+
+    // Update the game state by applying a game action
+    fn handle_game_action(&mut self, game_action: GameAction) {
+        match game_action {
+            GameAction::Move(direction) => self.move_player(direction),
+        }
     }
 }
 
@@ -32,8 +76,12 @@ impl Component for GameComponent {
         fb.set_cell_relative_to_ctx(ctx, state.player_coord, 0, render_cell_player);
     }
 
-    fn update(&mut self, _state: &mut Self::State, _ctx: Ctx, _event: Event) -> Self::Output {
-        // TODO: Update the game state when input is received
+    fn update(&mut self, state: &mut Self::State, _ctx: Ctx, event: Event) -> Self::Output {
+        if let Event::Input(input) = event {
+            if let Some(game_action) = game_action_from_input(input) {
+                state.handle_game_action(game_action);
+            }
+        }
     }
 
     fn size(&self, _state: &Self::State, ctx: Ctx) -> Size {
@@ -55,4 +103,5 @@ pub fn app() -> App {
             Err(Escape) => app::Exit, // Exit the program when escape is pressed.
             Ok(output) => output,     // Other outputs are simply returned.
         })
+        .clear_each_frame()
 }
